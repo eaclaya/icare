448e3d771f5303ffdac2a3d8ef19412249d7b9db
User <user@Users-MacBook-Pro.local>
2025-03-06 15:50:19 -0600

    Force redirect to filament dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 
-Route::view('/', 'welcome');
+// Force redirect to filament dashboard for now
+Route::redirect('/', '/admin');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
-// Route::get('sanctum/csrf-cookie', [CsrfCookieController::class, 'show']);
-// require __DIR__.'/auth.php';
